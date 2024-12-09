0bdf6b6d333629683cc02ad4dce0f37f81a9fd99
Gonveliz <gonzalojvelizpg@gmail.com>
2024-12-09 15:18:26 -0300

    agregando vpc modulo

--- eks.tf
+++ eks.tf
@@ -3,17 +3,17 @@ module "eks" {
   version = "20.31.0"
 
   cluster_name    = "challenge-eks"
-  cluster_version = "1.28"
-  vpc_id         = aws_vpc.main.id
-  subnet_ids     = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  cluster_version = "1.30"
+  vpc_id         = module.vpc.vpc_id
+  subnet_ids     = concat(module.vpc.public_subnets, module.vpc.private_subnets)
 
   cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
-    example = {
+    mng = {
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 2
+      desired_size = 1
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
--- vpc.tf
+++ vpc.tf
@@ -1,71 +1,17 @@
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-as-eks"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
-    Name = "main-vpc"
+    Terraform = "true"
+    Environment = "dev"
   }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-# Subnets públicas
-resource "aws_subnet" "public_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-    "kubernetes.io/cluster/mi-cluster-eks" = "shared"
-    "kubernetes.io/role/elb"               = "1"
-  }
-}
-
-resource "aws_subnet" "public_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-2"
-    "kubernetes.io/cluster/mi-cluster-eks" = "shared"
-    "kubernetes.io/role/elb"               = "1"
-  }
-}
-
-# Route Table para subnets públicas
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-# Asociaciones de Route Table
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
 }
