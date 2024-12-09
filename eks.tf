c059f6c9ad66e989c52e339ef7ece5017c6eddf5
Gonveliz <gonzalojvelizpg@gmail.com>
2024-12-09 19:27:03 -0300

    arreglando vpc modulo

--- eks.tf
+++ eks.tf
@@ -5,9 +5,10 @@ module "eks" {
   cluster_name    = "challenge-eks"
   cluster_version = "1.30"
   vpc_id         = module.vpc.vpc_id
-  subnet_ids     = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+  subnet_ids     = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     mng = {
