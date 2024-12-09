bda21e2f5034bae303d9244fb9501eb57aa2b696
Gonveliz <gonzalojvelizpg@gmail.com>
2024-12-09 14:35:47 -0300

    arreglando modulo de eks

--- eks.tf
+++ eks.tf
@@ -1,27 +1,24 @@
-# Modulo EKS
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "17.19.0"
+  version = "20.31.0"
 
   cluster_name    = "challenge-eks"
-  cluster_version = "1.24"
+  cluster_version = "1.28"
+  vpc_id         = aws_vpc.main.id
+  subnet_ids     = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
-  vpc_id     = aws_vpc.main.id
-  subnet_ids = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  cluster_endpoint_public_access = true
 
-  node_groups = {
+  eks_managed_node_groups = {
     example = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
     }
   }
-
-  manage_aws_auth = true
-  write_kubeconfig = true
 
   tags = {
     Environment = "dev"
